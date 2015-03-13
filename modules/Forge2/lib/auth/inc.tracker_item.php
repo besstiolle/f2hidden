45f8d561c9c77a7e1efe26e8f5c64f88bd010481
besstiolle <contact@furie.be>
2015-03-13 11:29:08 +0100

    remove old information

--- modules/Forge2/lib/auth/inc.tracker_item.php
+++ modules/Forge2/lib/auth/inc.tracker_item.php
@@ -5,10 +5,10 @@ if (!function_exists("cmsms")) exit;
 //TODO make constants and put everything into another file 
 //DEFINE('SID', '...');
 $fields = [
-	'sid' => '#^[0-9]+$#',
+/*	'sid' => '#^[0-9]+$#',
 	'project_id' => '#^[0-9]+$#',
 	'user_id' => '#^[0-9]+$#',
-	'role' => '#^[0-9]+$#',
+	'role' => '#^[0-9]+$#',*/
 
 	'sid' => '#^[0-9]+$#',
 	'project_id' => '#^[0-9]+$#',
